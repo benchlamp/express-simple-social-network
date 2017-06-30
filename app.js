2278e5e66877480549fb0e4073014f6d99d6c093
James <jamesrea83@gmail.com>
2017-06-30 15:37:20 +0000

    login/logout functionality added

--- app.js
+++ app.js
@@ -3,21 +3,26 @@ var mongoose = require("mongoose");
 var path = require("path");
 var bodyParser = require("body-parser");
 var cookieParser = require("cookie-parser");
+var passport = require("passport");
 var session = require("express-session");
 var flash = require("connect-flash");
 var routes = require("./routes");
+var setUpPassport = require("./setuppassport");
 var app = express();
 
-
 mongoose.connect("mongodb://localhost:27017/test");
+
+setUpPassport();
 
 app.set("port", process.env.PORT || 3000);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({
-    extended: false
-}));
+        extended: false
+    })
+);
+
 app.use(cookieParser());
 
 app.use(session({
@@ -27,6 +32,11 @@ app.use(session({
 }));
 
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 app.use(routes);
 
 app.listen(app.get("port"), function() {
--- routes.js
+++ routes.js
@@ -1,12 +1,16 @@
 var express = require("express");
 var User = require("./models/user");
 var router = express.Router();
+var passport = require("passport");
+
 router.use(function(req, res, next) {
     res.locals.currentUser = req.user;
     res.locals.errors = req.flash("error");
     res.locals.infos = req.flash("info");
     next();
 });
+
+
 router.get("/", function(req, res, next) {
     User.find()
         .sort({
@@ -21,4 +25,68 @@ router.get("/", function(req, res, next) {
             });
         });
 });
+
+
+router.get("/signup", function(req, res) {
+    res.render("signup");
+});
+router.post("/signup", function(req, res, next) {
+    var username = req.body.username;
+    var password = req.body.password;
+    User.findOne({
+        username: username
+    }, function(err, user) {
+        if (err) {
+            return next(err);
+        }
+        if (user) {
+            req.flash("error", "User already exists");
+            return res.redirect("/signup");
+        }
+        var newUser = new User({
+            username: username,
+            password: password
+        });
+        newUser.save(next);
+    });
+}, passport.authenticate("login", {
+    successRedirect: "/",
+    failureRedirect: "/signup",
+    failureFlash: true
+}));
+
+
+router.get("/users/:username", function(req, res, next) {
+    User.findOne({username: req.params.username}, function(err, user) {
+        if (err) {return next(err);}
+        if (!user) {return next(404);}
+        res.render("profile", {user: user});
+    });
+});
+
+
+router.get("/login", function(req, res) {
+    res.render("login");
+});
+
+router.post("/login", passport.authenticate("login", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true
+}));
+
+
+router.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/");
+});
+
+
+router.use(function(req, res, next) {
+    res.locals.currentUser = req.user;
+    res.locals.errors = req.flash("error");
+    res.locals.infos = req.flash("info");
+    next();
+});
+
 module.exports = router;

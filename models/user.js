55abe082026b74d9533516eb40d101c799500467
James <jamesrea83@gmail.com>
2017-06-30 13:17:42 +0000

    first test successful

--- models/user.js
+++ models/user.js
@@ -1,8 +1,6 @@
-var mongoose = require("mongoose");
 var bcrypt = require("bcrypt-nodejs");
+var mongoose = require("mongoose");
 var SALT_FACTOR = 10;
-
-
 var userSchema = mongoose.Schema({
     username: {
         type: String,
@@ -18,38 +16,35 @@ var userSchema = mongoose.Schema({
         default: Date.now
     },
     displayName: String,
-    bio: String
+    bio: String,
 });
-
 var noop = function() {};
-
 userSchema.pre("save", function(done) {
     var user = this;
-    
-    if(!user.isModified("password")) {
+    if (!user.isModified("password")) {
         return done();
     }
     bcrypt.genSalt(SALT_FACTOR, function(err, salt) {
-        if (err) { return done(err); }
+        if (err) {
+            return done(err);
+        }
         bcrypt.hash(user.password, salt, noop, function(err, hashedPassword) {
-            if (err) { return done(err); }
+            if (err) {
+                return done(err);
+            }
             user.password = hashedPassword;
             done();
         });
     });
 });
 
-
 userSchema.methods.checkPassword = function(guess, done) {
     bcrypt.compare(guess, this.password, function(err, isMatch) {
         done(err, isMatch);
     });
 };
-
 userSchema.methods.name = function() {
     return this.displayName || this.username;
-}
-
+};
 var User = mongoose.model("User", userSchema);
-
 module.exports = User;
